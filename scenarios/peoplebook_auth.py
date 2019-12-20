77673212e68edd36d923db1c8219eb571b48a788
David Dale <ddale@yandex-team.ru>
2019-12-20 16:13:45 +0300

    fix peoplebook url

--- scenarios/peoplebook_auth.py
+++ scenarios/peoplebook_auth.py
@@ -9,7 +9,7 @@ def make_pb_url(relative_path, user_tg_id):
         relative_path = relative_path[1:]
     new_id = str(user_tg_id) + os.environ.get('login_salt')
     auth_token = hashlib.md5(str(new_id).encode('utf-8')).hexdigest(),
-    return '{}/login_link?bot_info={}&next={}">'.format(
+    return '{}/login_link?bot_info={}&next={}'.format(
         PEOPLEBOOK_ROOT,
         auth_token,
         relative_path,
